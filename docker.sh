e49b275596c5fea605594a14a06593d5ee71b084
tacomoon <evlgorbachev@gmail.com>
2018-11-14 10:53:49 +0300

    Improve docker shell script

--- docker.sh
+++ docker.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
 
 NC='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-ACCESS_TOKEN='paste_your_access_token_here'
+ACCESS_TOKEN=''
 
 function log_message() {
     echo -e ${1}
 }
 
 function log_error() {
-    echo -e "${RED}${1}${NC}" >&2
+    echo -e ${RED}${1}${NC} >&2
 }
 
 function print_help() {
-    echo -e "Commands:"
+    echo -e "\nCommands:"
     echo -e "  b,  build\tBuild and tag image as latest"
     echo -e "  r,  run\tRun image tagged as latest"
     echo -e "  s,  stop\tStop running container"
@@ -30,122 +29,140 @@ function print_help() {
     echo -e "  --with-node\tDelete node image from docker too"
 }
 
-function build_container() {
-    VERSION="$(date +'%s')"
-    docker build -t argue-bot:${VERSION} .
-    docker tag argue-bot:${VERSION} argue-bot:latest
+function find_running_container() {
+    CONTAINER_ID=$(docker ps -q -f=ancestor=argue-bot:latest)
 }
 
-function run_container() {
-    stop_container 2> /dev/null
-    log_message "starting container"
-    docker run -d -p 8080:3000 -e ACCESS_TOKEN=${ACCESS_TOKEN} argue-bot:latest
+function check_access_token {
+    if [ -z ${ACCESS_TOKEN} ]; then
+        log_error 'ACCESS_TOKEN is required, visit https://vk.com/dev/access_token to get one'
+        exit 1
+    fi
 }
 
-function stop_container() {
-    CONTAINER_ID="$(docker ps -q -f=ancestor=argue-bot:latest)"
-    if [ -z "${CONTAINER_ID}" ]; then
-        log_error "can't find running container"
-        return 1
+function check_container_running {
+    if [ -z ${CONTAINER_ID} ]; then
+        find_running_container
     fi
 
-    ! [ -z "${CONTAINER_ID}" ] && {
-        log_message "stopping container $CONTAINER_ID"
+    if [ -z ${CONTAINER_ID} ]; then
+        log_error "Can't find running digital-diary database container"
+        exit 1
+    fi
+}
+
+function soft_stop_container {
+    find_running_container
+
+    if [ ${CONTAINER_ID} ]; then
+        log_message "Stopping running container $CONTAINER_ID"
         docker stop ${CONTAINER_ID}
-    }
+    fi
 }
 
-function connect_to_container() {
-    CONTAINER_ID="$(docker ps -q -f=ancestor=argue-bot:latest)"
-    if [ -z "${CONTAINER_ID}" ]; then
-        log_error "can't find running container"
-        return 1
-    fi
-
-    ! [ -z "${CONTAINER_ID}" ] && {
-        docker exec -it ${CONTAINER_ID} bash
-    }
-}
-
-function get_logs_from_container() {
-    CONTAINER_ID="$(docker ps -q -f=ancestor=argue-bot:latest)"
-    if [ -z "${CONTAINER_ID}" ]; then
-        log_error "can't find running container"
-        return 1
-    fi
-
+function build_docker_log_query {
     tail=false
     follow=false
-
     shift
+
     while [[ true ]]; do
         case ${1} in
             --tail )
-                tail=true; shift
+                tail=true
+                shift
                 ;;
             --follow )
-                follow=true; shift
+                follow=true
+                shift
                 ;;
             * )
-                if ! [[ ${1} ]]; then break; fi;
+                if [ -z ${1} ]; then break; fi
 
-                log_error "unknown flag: ${1} \n"
+                log_error "Unknown flag: ${1}"
                 print_help
-                return 1
+                exit 1
         esac
     done
 
     DOCKER_CMD="docker logs"
     if ${tail}; then
-        DOCKER_CMD="${DOCKER_CMD} --tail 50"
+        DOCKER_CMD=${DOCKER_CMD} --tail 50
     fi
     if ${follow}; then
-        DOCKER_CMD="${DOCKER_CMD} --follow"
+        DOCKER_CMD=${DOCKER_CMD} --follow
     fi
-    DOCKER_CMD="${DOCKER_CMD} ${CONTAINER_ID}"
-
-    eval ${DOCKER_CMD}
+    DOCKER_CMD=${DOCKER_CMD} ${CONTAINER_ID}
 }
 
 function clean_docker() {
     with_node=false
-
     shift
+
     if [[ ${1} ]]; then
         case ${1} in
             --with-node )
                 with_node=true
+                shift
                 ;;
         * )
-            log_error "unknown flag: ${1} \n"
+            if [ -z ${1} ]; then break; fi
+
+            log_error "unknown flag: ${1}"
             print_help
-            return 1
+            exit 1
         esac
     fi
 
-    stop_container 2> /dev/null
-    log_message "removing related containers"
-    docker rm -f $(docker ps -qa -f ancestor=argue-bot) 2> /dev/null
-    log_message "removing related images"
-    docker rmi -f $(docker images argue-bot -q) 2> /dev/null
-
+    # Removing related containers
+    DOCKER_CMD=docker rm -f $(docker ps -qa -f ancestor=argue-bot) 2> /dev/null
+    # Removing related images
+    DOCKER_CMD=${DOCKER_CMD} && docker rmi -f $(docker images argue-bot -q) 2> /dev/null
+    # Removing node
     if ${with_node}; then
-        log_message "removing related node"
-        docker rmi node:10.8.0
+        DOCKER_CMD=${DOCKER_CMD} && docker rmi node:10.8.0
     fi
 }
 
 case ${1} in
-    b | build ) build_container ;;
-    r | run ) run_container ;;
-    s | stop ) stop_container;;
-    c | connect ) connect_to_container ;;
-    l | logs ) get_logs_from_container $@ ;;
-    cl | clean ) clean_docker $@ ;;
+
+    b | build )
+        VERSION=$(date +'%s')
+        DOCKER_CONTEXT=$(dirname ${0})
+
+        docker build -t argue-bot:${VERSION} ${DOCKER_CONTEXT}
+        docker tag argue-bot:${VERSION} argue-bot:latest
+        ;;
+
+    r | run )
+        check_access_token
+        soft_stop_container
+        log_message "starting container"
+        docker run -d -p 8080:3000 -e ACCESS_TOKEN=${ACCESS_TOKEN} argue-bot:latest
+        ;;
+
+    c | connect )
+        check_container_running
+        docker exec -it ${CONTAINER_ID} bash
+        ;;
+
+    s | stop )
+        check_container_running
+        soft_stop_container
+        ;;
+
+    l | logs )
+        check_container_running
+        build_docker_log_query $@
+        eval ${DOCKER_CMD}
+        ;;
+
+    cl | clear )
+        soft_stop_container
+        clean_docker $@
+        eval ${DOCKER_CMD}
+        ;;
+
     * )
-        if [[ ${1} ]]; then
-            log_error "unknown command: ${1} \n"
-        fi
+        if [ ${1} ]; then log_error "unknown command: ${1}"; fi
         print_help
-    ;;
 esac
